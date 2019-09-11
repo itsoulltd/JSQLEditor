c3eb9ec18d41f172c6e68dccce778388760eb4ca
Towhid Islam <m.towhid.islam@gmail.com>
2019-09-11 19:53:47 +0600

    updated

--- src/main/java/com/it/soul/lab/connect/DriverClass.java
+++ src/main/java/com/it/soul/lab/connect/DriverClass.java
@@ -5,122 +5,44 @@ import java.util.EnumSet;
 import java.util.List;
 
 public enum DriverClass {
-    MYSQL,
-    PostgraySQLv7,
-    DB2,
-    OracleOCI9i,
-    SQLServer,
-    H2_MEM,
-    H2_FILE,
-    H2_SERVER,
-    H2_SERVER_TLS,
-    NONE;
+
+    MYSQL("com.mysql.jdbc.Driver","jdbc:mysql://","3306"),
+    PostgresQLv7("org.postgresql.Driver","jdbc:postgresql://","5432"),
+    DB2("COM.ibm.db2.jdbc.app.DB2Driver","jdbc:db2://","446"),
+    OracleOCI9i("oracle.jdbc.driver.OracleDriver","jdbc:oracle:thin:@//",""),
+    SQLServer("com.microsoft.jdbc.sqlserver.SQLServerDriver","jdbc:sqlserver://","1433"),
+    H2_EMBEDDED("org.h2.Driver","jdbc:h2:mem:",""),
+    H2_FILE("org.h2.Driver","jdbc:h2:file:",""),
+    H2_SERVER("org.h2.Driver","jdbc:h2:tcp://","8084"),
+    H2_SERVER_TLS("org.h2.Driver","jdbc:h2:ssl://","8085"),
+    HSQL_EMBEDDED("org.hsqldb.jdbcDriver","jdbc:hsqldb:hsql://","9001"),
+    DERBY_EMBEDDED("org.apache.derby.jdbc.EmbeddedDriver","jdbc:derby://","1527"),
+    JDBC_ODBC("sun.jdbc.odbc.JdbcOdbcDriver","jdbc:odbc://","");
+
+    private final String driverClassName;
+    private final String urlSchema;
+    private final String defaultPort;
+
+    DriverClass(String driverClassName, String urlSchema, String defaultPort) {
+        this.driverClassName = driverClassName;
+        this.urlSchema = urlSchema;
+        this.defaultPort = defaultPort;
+    }
 
     public String toString(){
-        String result = "";
-        switch (this) {
-        case MYSQL:
-            result = "com.mysql.jdbc.Driver";
-            break;
-        case PostgraySQLv7:
-            result = "org.postgresql.Driver";
-            break;
-        case DB2:
-            result = "COM.ibm.db2.jdbc.app.DB2Driver";
-            break;
-        case OracleOCI9i:
-            result = "oracle.jdbc.driver.OracleDriver";
-            break;
-        case SQLServer:
-            result = "com.microsoft.jdbc.sqlserver.SQLServerDriver";
-            break;
-        case H2_MEM:
-        case H2_FILE:
-        case H2_SERVER:
-        case H2_SERVER_TLS:
-            result = "org.h2.Driver";
-            break;
-        default:
-            result = "sun.jdbc.odbc.JdbcOdbcDriver";
-            break;
-        }
-        return result;
+        return driverClassName;
     }
 
     public String urlSchema() {
-        String result = "";
-        switch (this) {
-        case MYSQL:
-            result = "jdbc:mysql://";
-            break;
-        case PostgraySQLv7:
-            result = "jdbc:postgresql://";
-            break;
-        case DB2:
-            result = "jdbc:db2://";
-            break;
-        case OracleOCI9i:
-            result = "jdbc:oracle:thin:@//";
-            break;
-        case SQLServer:
-            result = "jdbc:sqlserver://";
-            break;
-        case H2_MEM:
-            result = "jdbc:h2:mem:";
-            break;
-        case H2_FILE:
-            result = "jdbc:h2:file:";
-            break;
-        case H2_SERVER:
-            result = "jdbc:h2:tcp://";
-            break;
-        case H2_SERVER_TLS:
-            result = "jdbc:h2:ssl://";
-            break;
-        default:
-            result = "jdbc:odbc:";
-            break;
-        }
-        return result;
+        return urlSchema;
     }
 
     public String defaultPort() {
-        String result = "";
-        switch (this) {
-        case MYSQL:
-            result = "3306";
-            break;
-        case PostgraySQLv7:
-            result = "5432";
-            break;
-        case DB2:
-            result = "446";
-            break;
-        case OracleOCI9i:
-            result = "";
-            break;
-        case SQLServer:
-            result = "1433";
-            break;
-        case H2_MEM:
-        case H2_FILE:
-            result = "";
-            break;
-        case H2_SERVER:
-            result = "8084";
-            break;
-        case H2_SERVER_TLS:
-            result = "8085";
-            break;
-        default:
-            result = "";
-            break;
-        }
-        return result;
+        return defaultPort;
     }
 
     public static DriverClass getMatchedDriver(String connectionURL) {
-        DriverClass result = NONE;
+        DriverClass result = JDBC_ODBC;
         List<DriverClass> all = new ArrayList<>(EnumSet.allOf(DriverClass.class));
         for (DriverClass driverClass : all) {
             if (connectionURL.startsWith(driverClass.urlSchema())) {
